7ca2995d329676860ba8c464c31f675387f140bc
hfesg <xuyankun421@163.com>
2017-05-08 14:54:26 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/example/ykx/duocaiweather/db/Province.java
@@ -0,0 +1,38 @@
+package com.example.ykx.duocaiweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by YKX on 2017/5/8.
+ */
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public int getId() {
+
+        return id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+}
